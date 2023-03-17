5977c34001d9063a00dedb709295104cd81476d8
your name goes here <jarolimfilip07@gmail.com>
2023-03-17 23:59:51 +0100

    added first content

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import "./index.css"
+import {BrowserRouter, Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className={"w-10 h-10 bg-black"}>
+
+      </div>
+
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
